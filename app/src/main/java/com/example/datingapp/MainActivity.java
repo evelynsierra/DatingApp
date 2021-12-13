43bacaa5375ebf130b69140dead0fe937ac4928c
evelynsierra <sierravelin@gmail.com>
2021-12-13 17:42:09 +0700

    Sign up connected to firebase

--- app/src/main/java/com/example/datingapp/MainActivity.java
+++ app/src/main/java/com/example/datingapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.datingapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.example.datingapp.adapter.viewPagerAdapter;
@@ -14,11 +18,13 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager mViewPager;
     private TabLayout mTabs;
     private viewPagerAdapter adapter;
+  //  private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         mViewPager = findViewById(R.id.viewPager);
         mTabs = findViewById(R.id.tabLayout);
@@ -29,5 +35,31 @@ public class MainActivity extends AppCompatActivity {
 
         mViewPager.setAdapter(adapter);
         mTabs.setupWithViewPager(mViewPager);
+
+        //saat menekan sign in atau sign up, backgroundnya akan berubah
+        mTabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if(tab.getPosition() == 0) {
+                    findViewById(R.id.layout_main).setBackground(getDrawable(R.drawable.layout_bg)); //saat menekan sign in, muncul layout_bg
+                    getWindow().setStatusBarColor(getColor(R.color.colorPrimary));
+                }
+                if(tab.getPosition() == 1) {
+                    findViewById(R.id.layout_main).setBackground(getDrawable(R.drawable.signup_bg)); //saat menekan sign up, muncul signup_bg
+                    getWindow().setStatusBarColor(getColor(R.color.colorGolden));
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/datingapp/fragment/SignUpFragment.java
+++ app/src/main/java/com/example/datingapp/fragment/SignUpFragment.java
@@ -1,66 +1,104 @@
 package com.example.datingapp.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.datingapp.HomeActivity;
 import com.example.datingapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SignUpFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.jetbrains.annotations.NotNull;
+
+///**
+// * A simple {@link Fragment} subclass.
+// * Use the {@link SignUpFragment#newInstance} factory method to
+// * create an instance of this fragment.
+// */
 public class SignUpFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+//    private String mParam1;
+//    private String mParam2;
+    private FirebaseAuth mAuth;
+    private EditText mName;
+    private EditText mEmail;
+    private EditText mPassword;
+    private Button mSignUpBtn;
 
     public SignUpFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SignUpFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SignUpFragment newInstance(String param1, String param2) {
-        SignUpFragment fragment = new SignUpFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+//    // TODO: Rename and change types and number of parameters
+//    public static SignUpFragment newInstance(String param1, String param2) {
+//        SignUpFragment fragment = new SignUpFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        mAuth = FirebaseAuth.getInstance();
+        mName = view.findViewById(R.id.signup_name);
+        mEmail = view.findViewById(R.id.signup_email);
+        mPassword = view.findViewById(R.id.signup_password);
+        mSignUpBtn = view.findViewById(R.id.signup_btn);
+
+        mSignUpBtn.setOnClickListener(new View.OnClickListener() { // saat diklik, tulis apa yang akan terjadi
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) { //jika akun berhasil dibuat
+                            Intent intent = new Intent(getContext(), HomeActivity.class); //akan diarahkan ke HomeActivity -> mengarah ke halaman home
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            //getContext().finish();
+                        } else {
+                            Toast.makeText(getContext(), ""+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        return view;
+
     }
 }
